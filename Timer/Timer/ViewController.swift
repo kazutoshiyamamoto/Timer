263a76063190432c27cdab15bb6802c811ab0722
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-02-22 23:58:57 +0900

    タイマーから定期的に呼び出されるメソッド

--- Timer/Timer/ViewController.swift
+++ Timer/Timer/ViewController.swift
@@ -22,15 +22,27 @@ class ViewController: UIViewController {
             target: self,
             selector: #selector(self.step),
             userInfo: nil,
-            repeats: true) // リピート再生する
+            repeats: true // リピート再生する
         )
+    }
+    
+    // タイマーから定期的に呼び出されるメソッド
+    @objc func step(){
+        // 水平方向へ移動
+        car.center.x += 10
+        // 右辺から外へ出たら
+        let carWidth = car.bounds.width
+        if car.center.x>(view.bounds.width + carWidth/2) {
+            // 左辺の手前に戻す
+            car.center.x = -carWidth
+            // y座標はランダムな高さに変更
+            let viewH = view.bounds.height
+             car.center.y = CGFloat(arc4random_uniform(UInt32(viewH)))
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
